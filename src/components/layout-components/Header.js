1c03f79fd16b9f7b8a398278c2571cf194358ec0
Илья Сёмин <semin574@yandex.ru>
2023-11-06 17:53:49 +0300

    Сверстал компоненты главная и редактирвать профиль. Доработал компонент header

--- src/components/layout-components/Header.js
+++ src/components/layout-components/Header.js
@@ -1,10 +1,13 @@
 import styles from "./Header.module.css";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Container from "./Container";
 import Logo from "../../images/logo.svg";
 import BasketLink from "../basket-components/BasketLink";
+import ProfileLink from "../users-components/ProfileLink";
 
-const setActive = ({isActive}) => isActive ? "active__link" : '';
+const setActive = ({ isActive }) => isActive ? "active__link" : '';
+
+const isAuth = true;
 
 const Header = () => {
 
@@ -17,14 +20,25 @@ const Header = () => {
                 <nav className={styles["navigation"]}>
                     <ul>
                         <li>
-                            <NavLink className={setActive} to="/">О нас</NavLink>
+                            <NavLink className={setActive} to="/">Главная</NavLink>
                         </li>
                         <li>
                             <NavLink className={setActive} to="/products">Каталог товаров</NavLink>
                         </li>
                     </ul>
                 </nav>
-                <BasketLink />
+                {!isAuth &&
+                    <div className={styles["auth__links"]}>
+                        <Link to="/auth/login">Войти</Link>
+                        <Link to="/auth/register">Зарегистрироваться</Link>
+                    </div>
+                }
+                {isAuth &&
+                    <div className={styles["profile__links"]}>
+                        <BasketLink />
+                        <ProfileLink />
+                    </div>
+                }
             </Container>
         </header>
     );
--- src/components/users-components/UserInfoEdit.js
+++ src/components/users-components/UserInfoEdit.js
@@ -1,8 +1,90 @@
 import React from 'react';
+import styles from "./UserInfoEdit.module.css";
 
 const UserInfoEdit = () => {
+
+
+
+
+
   return (
-    <div></div>
+    <div className={styles["profile__edit"]}>
+      <form className={styles["profile__edit_wrapper"]}>
+        <div className={styles["profile__edit_general-info"]}>
+          <h2>Общая инофрмация</h2>
+          <div className={styles["edit__sections"]}>
+            <div className={styles["edit__section"]}>
+              <div className={styles["edit__item"]}>
+                <label className={styles["edit__item_label"]} htmlFor='surname'>
+                  <p>Фамилия:</p>
+                  <input id='surname' type='text' />
+                </label>
+              </div>
+              <div className={styles["edit__item"]}>
+                <label className={styles["edit__item_label"]} htmlFor='name'>
+                  <p>Имя:</p>
+                  <input id='name' type='text' />
+                </label>
+              </div>
+              <div className={styles["edit__item"]}>
+                <label className={styles["edit__item_label"]} htmlFor='patronymic'>
+                  <p>Отчество:</p>
+                  <input id='patronymic' type='text' />
+                </label>
+              </div>
+            </div>
+            <div className={styles["edit__section"]}>
+              <div className={styles["edit__item"]}>
+                <label className={styles["edit__item_label"]} htmlFor='dateOfBirth'>
+                  <p>Дата рождения:</p>
+                  <input id='dateOfBirth' type="date" />
+                </label>
+              </div>
+              <div className={styles["edit__item"]}>
+                <label className={styles["edit__item_label"]} htmlFor='gender'>
+                  <p>Пол:</p>
+                  <select id='gender'>
+                    <option>мужчина</option>
+                    <option>женщина</option>
+                  </select>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles["profile__edit_contacts-info"]}>
+          <h2>Контактная информация</h2>
+          <div className={styles["edit__sections"]}>
+            <div className={styles["edit__section"]}>
+              <div className={styles["edit__item"]}>
+                <label className={styles["edit__item_label"]} htmlFor='email'>
+                  <p>Эл. почта:</p>
+                  <input id='email' type="email" />
+                </label>
+              </div>
+              <div className={styles["edit__item"]}>
+                <label className={styles["edit__item_label"]} htmlFor='tel'>
+                  <p>Номер телефона:</p>
+                  <input id='tel' type='tel' />
+                </label>
+              </div>
+              <div className={styles["edit__item"]}>
+                <label className={styles["edit__item_label"]} htmlFor='address'>
+                  <p>Адрес:</p>
+                  <input id='address' type="text" />
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles["edit__actions"]}>
+          <button className={styles["edit__cancel"]}>Отмена</button>
+          <button className={styles["edit__save"]}>Сохранить</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
